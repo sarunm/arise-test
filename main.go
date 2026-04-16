c1b13b86da97d0d4ac311c76289ac5d5dd874f90
S. <sarun.maksuanpan@gmail.com>
2026-04-16 16:24:49 +0700

    update main

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/sarunm/arise-test/internal/account"
+	"github.com/sarunm/arise-test/internal/customer"
+	"github.com/sarunm/arise-test/internal/transaction"
+	"github.com/sarunm/arise-test/pkg/database"
+)
+
+func main() {
+	// Database
+	db, err := database.New()
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	// Modules
+	customerMod    := customer.NewModule(db)
+	accountMod     := account.NewModule(db)
+	transactionMod := transaction.NewModule(db)
+
+	// Router
+	r := gin.Default()
+
+	v1 := r.Group("/api/v1")
+	customerMod.RegisterRoutes(v1)
+	accountMod.RegisterRoutes(v1)
+	transactionMod.RegisterRoutes(v1)
+
+	// Start
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	if err := r.Run(":" + port); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
 }
