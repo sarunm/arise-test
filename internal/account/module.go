81f5b0f32f36d87856b113322c3f37477e6f07a4
S. <sarun.maksuanpan@gmail.com>
2026-04-16 16:24:37 +0700

    add module and database

---
+++ internal/account/module.go
@@ -0,0 +1,26 @@
+package account
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Module struct {
+	Service Service
+	Handler *Handler
+}
+
+func NewModule(db *gorm.DB) *Module {
+	repo    := newRepo(db)
+	service := NewService(repo)
+	handler := NewHandler(service)
+
+	return &Module{
+		Service: service,
+		Handler: handler,
+	}
+}
+
+func (m *Module) RegisterRoutes(r *gin.RouterGroup) {
+	m.Handler.RegisterRoutes(r)
+}
